0754738bbab0f8c122cd93438ce7da04c1397cd9
terceirobapita <terceirobapitaa@gmail.com>
2023-04-14 19:01:14 -0300

    fixed some errors

---
+++ README.md
@@ -0,0 +1,23 @@
+# Gerenciador de arquivos
+
+## About
+> Um simples gerenciador de arquivos
+
+## Functions
+> Exibe todos os arquivos em uma pasta selecionada pelo usuário
+
+> Possue a opção de seleção e remoção dos arquivos selecionados pelo usuário
+
+> Possue a opção de exportação dos arquivos selecionados pelo usuário
+
+> Filtra os arquivos com base nas extensões (.jpg, .mp4, etc.)
+
+> Exibe uma miniatura do arquivo selecionado caso o mesmo seja uma imagem
+
+> Exibe a quantidade de arquivos dentro da pasta selecionada
+
+>![image](https://user-images.githubusercontent.com/98183878/193454033-a9108755-14f0-4a08-88ae-47998ea3c290.png)
+>![image](https://user-images.githubusercontent.com/98183878/193454060-9b7fa70a-71c1-48bd-82f3-d78308257aa3.png)
+>![image](https://user-images.githubusercontent.com/98183878/193454088-7812f3b5-3194-4a33-b2ad-a5ab9530e35c.png)
+>![image](https://user-images.githubusercontent.com/98183878/193454078-73cff332-00b8-40ae-a72b-09ad1aac436f.png)
+>![image](https://user-images.githubusercontent.com/98183878/193454098-00a51960-e43e-4343-b9bd-135f99036c20.png)
---
+++ cleaner.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import shutil
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QMessageBox
+
+if not os.path.exists(os.getcwd()+'\exported'):
+    os.mkdir(os.getcwd()+'\exported')
+
+root = os.getcwd()
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self, parent=None): # Configura a janela
+        super(Ui, self).__init__(parent=parent)
+        uic.loadUi('cleaner.ui', self)
+        self.directoryinput.setText(os.getcwd())
+        self.show()
+        self.listar()
+        
+        # Conexão dos botões com os eventos
+        self.list.itemSelectionChanged.connect(self.on_change) 
+        self.filter.activated.connect(self.listar)
+        self.erase.clicked.connect(self.remove)
+        self.directorySelect.clicked.connect(self.get_directory)
+        self.search.clicked.connect(self.get_directory_input)
+        self.export_2.clicked.connect(self.export)
+        
+    def get_directory(self): # Retorna o caminho da pasta selecionada
+        file = str(QFileDialog.getExistingDirectory(self, "Selecione uma pasta"))
+        if not file:
+            file = os.getcwd()
+        else:   
+            os.chdir(file)
+            self.listar()
+        
+        self.directoryinput.setText(file)
+        return file
+    
+    def get_directory_input(self):
+        file = self.directoryinput.text()
+        if file == '':
+            pass
+        else:
+            try:
+                os.chdir(file)
+            except FileNotFoundError:
+                pass
+            self.listar()
+        
+    def listar(self): # Lista todos os arquivos que a pasta possue
+        combo = self.filter.currentText()
+        lista = self.list
+        files = os.listdir()
+        self.total.setText(f'Total de arquivos: {str(len(files))}')
+        
+        # Filtra os arquivos
+        if combo == 'Todos':
+                lista.clear()
+                for file in files:
+                    lista.addItem(file)
+                   
+        else:
+            lista.clear()
+            
+            num = 0
+            for file in files:
+                if combo in file:
+                    lista.addItem(file)
+                    num += 1
+                self.total.setText(f'Total de arquivos: {str(num)}') 
+            else:
+                lista.addItem(f"""Não existem arquivos ".{combo}" nesta pasta. """)
+                 
+            
+
+    def on_change(self): # Retorna os arquivos selecionados e exibe a respectiva imagem
+        if len([item.text() for item in self.list.selectedItems()]) > 1:
+            pass
+        else:
+            for item in self.list.selectedItems():
+                for a in [item.text()]:
+                    try:
+                        self.picture.setPixmap(QtGui.QPixmap(a).scaled(400, 1000, QtCore.Qt.KeepAspectRatio))
+                    except:
+                        pass
+        
+        return [item.text() for item in self.list.selectedItems()]
+        
+
+    def remove(self): # Remove os arquivos selecionados
+        items = Ui.on_change(self)
+        for item in items:
+            # Exibe uma janela de confirmação
+            q = QMessageBox.question(self, 'PyQt5 message', f"Deseja remover {item}?", QMessageBox.Yes | QMessageBox.No)
+            if q == QMessageBox.Yes:
+                try:
+                    os.remove(item)
+                    self.list.clear()
+                    self.listar()
+                except PermissionError:
+                    q = QMessageBox.question(self, 'Permission Error', f"O aplicativo não possue permissão para remover {item}", QMessageBox.Ok)
+            else:
+                pass
+    
+    def export(self): 
+        items = Ui.on_change(self)
+        for item in items:
+            q = QMessageBox.question(self, 'PyQt5 message', f"Deseja exportar {item}?", QMessageBox.Yes | QMessageBox.No)
+            
+            if q == QMessageBox.Yes:
+                shutil.copy(item, f'{root}\exported')
+            else:
+                pass
+    
+
+app = QApplication(sys.argv)
+window = Ui()
+app.exec_()
